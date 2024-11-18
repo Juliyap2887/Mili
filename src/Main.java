8293c2103298b28e6c2493945955fb5b95d8e330
Juliya <Juliyap2887@mail.ru>
2024-11-18 19:58:10 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 14897;
+        int oneMiles = 20;
+        int miles = amount / oneMiles;
+
+        System.out.println(miles);
+
+        }
+    }
